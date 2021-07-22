4d84d02285909f9165c1cc4f618ffb82ada8c534
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2021-07-23 01:51:37 +0900

    Create 18436.cpp

---
+++ BOJ/세그먼트 트리/18436.cpp
@@ -0,0 +1,75 @@
+//Basic Segment tree
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+//Fast query/modify from codeforces http://codeforces.com/blog/entry/18051
+long long query(vector<long long>& t, int n, int l, int r) {  // sum on interval [l, r)
+	long long res = 0;
+	for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+		if (l & 1) res += t[l++];
+		if (r & 1) res += t[--r];
+	}
+	return res;
+}
+
+void modify(vector<long long>& t, int n, int p, long long value) {  // set value at position p
+	for (t[p += n] = value; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
+}
+
+void build(vector<long long>& t, int n) {  // build the tree
+	for (int i = n - 1; i > 0; --i) t[i] = t[i << 1] + t[i << 1 | 1];
+}
+
+int main()
+{
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	int h = ceil(log2(N));
+	int szTree = (1 << (h + 1));
+	vector<long long> segTree(szTree);
+
+	int val;
+	for (int i = 0; i < N; i++)//O(n)
+	{
+		cin >> val;
+		segTree[(szTree >> 1) + i] = val % 2;
+	}
+
+	build(segTree, szTree >> 1);
+
+	int M;
+	cin >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		long long a, b, c;
+		cin >> a >> b >> c;
+
+		if (a == 1)//update
+		{
+			modify(segTree, szTree >> 1, b - 1, c % 2);
+		}
+		else if(a == 2)//even
+		{
+			cout << c - b + 1 - query(segTree, szTree >> 1, b - 1, c) << '\n';
+		}
+		else//odd
+		{
+			cout << query(segTree, szTree >> 1, b - 1, c) << '\n';
+		}
+	}
+
+	return 0;
+
+}
